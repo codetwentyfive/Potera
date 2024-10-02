6c362cad2f2fa46505e56c0596430dbc557137fb
code25 <chingisenkhbaatar@gmail.com>
2024-10-02 17:01:32 +0200

    Contact Info creation!

---
+++ components/ContactInfo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { PhoneIcon, MailIcon } from '@heroicons/react/outline';
+import Image from 'next/image';
+
+const ContactInfo: React.FC = () => {
+  return (
+    <div className="flex items-end justify-end space-x-4 text-sm">
+      <a className="flex items-center text-gray-700 hover:text-blue-600" href="tel:+4917632747881">
+        <Image src="/icons/mobile.svg" alt="Mobile" width={16} height={16} className="mr-1" />
+        +49 176 3274 7881
+      </a>
+      <a className="flex items-center text-gray-700 hover:text-blue-600" href="tel:+49092213950562">
+        <PhoneIcon className="h-4 w-4 mr-1" />
+        +49 0922139 50562
+      </a>
+      <a className="flex items-center text-gray-700 hover:text-blue-600" href="mailto:kontakt@poterareinigung.de">
+        <MailIcon className="h-4 w-4 mr-1" />
+        kontakt@poterareinigung.de
+      </a>
+    </div>
+  );
+};
+
+export default ContactInfo;
